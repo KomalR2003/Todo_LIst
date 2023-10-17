bedb507caa77da26ff10ee9c942198b321dbffc9
KomalR2003 <komalribadiya0106@gmail.com>
2023-10-17 15:56:07 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+//components
+import Header from './components/Header';
+import TodoForm from './components/TodoForm';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    
+      <>
+   <Header/>
+   <TodoForm/>
+   <Todos/>
+      </>
+  );
+}
+
+export default App;
